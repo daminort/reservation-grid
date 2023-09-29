ecbb2976d3e54182eee415f46391ed4e42faa5eb
incode <you@example.com>
2023-09-29 17:34:41 +0300

    Fix export types

--- src/plugins/postBuild/postBuild.ts
+++ src/plugins/postBuild/postBuild.ts
@@ -2,6 +2,13 @@ import { join, isAbsolute } from 'node:path';
 import { cp, mkdir, rm } from 'fs/promises';
 import type { Plugin } from 'vite';
 
+const files = [
+  'dist/vite.common.d.ts',
+  'dist/vite.config.d.ts',
+  'dist/vite.config-gh.d.ts',
+  'dist/jest.config.d.ts',
+];
+
 const toAbsolutePath = (path: string) => {
   const cwd = process.cwd();
   if (!isAbsolute(path)) {
@@ -29,12 +36,15 @@ function createPlugin(): Plugin {
     const src = toAbsolutePath('dist/src');
     const srcLib = toAbsolutePath('dist/src/lib');
     const destLib = toAbsolutePath('dist/lib');
-    const configPath = toAbsolutePath('dist/vite.config.d.ts');
 
     await mkdir(destLib);
     await cp(srcLib, destLib, { recursive: true });
     await rm(src, { recursive: true });
-    await rm(configPath);
+
+    for (const fileName of files) {
+      const filePath = toAbsolutePath(fileName);
+      await rm(filePath);
+    }
 
     const expired = Number((Date.now() - startTime) / 1000);
 
